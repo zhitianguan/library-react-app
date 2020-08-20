4b6ac76bfb812ff3bad23522d2b0852e540a2b54
Zhitian Guan <zguan.university@gmail.com>
2020-08-20 17:26:21 -0400

    Updates

---
+++ precache-manifest.7b46123b6efefa9ed9fc17cd4ca053dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b960b653707848609adf1c8dbe0bb0",
+    "url": "/library-react-app/index.html"
+  },
+  {
+    "revision": "644bf502c9e90328e24f",
+    "url": "/library-react-app/static/css/main.7563da38.chunk.css"
+  },
+  {
+    "revision": "c77a0de02d7895364173",
+    "url": "/library-react-app/static/js/2.96735d4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-react-app/static/js/2.96735d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644bf502c9e90328e24f",
+    "url": "/library-react-app/static/js/main.f1065c12.chunk.js"
+  },
+  {
+    "revision": "47527dfaaafbeb45684c",
+    "url": "/library-react-app/static/js/runtime-main.87cda7f3.js"
+  },
+  {
+    "revision": "a5e3913c34d0df741257435a05b4f80d",
+    "url": "/library-react-app/static/media/janko-ferlic-sfL_QOnmy00-unsplash.a5e3913c.jpg"
+  }
+]);
